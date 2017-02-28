f059f2ebca5ed2e6acb40f67f0e5d534efe21f4a
Kelly Hoffer <kellyhoffer@Kellys-MacBook-Pro-2.local>
2017-02-28 13:06:54 -0800

    getting meeting time tuples sorted and identitfying how to compare tuples. Working on how to merge times now

--- intCake4.py
+++ intCake4.py
@@ -1,52 +1,66 @@
-# Your company built an in-house calendar tool called HiCal. 
-# You want to add a feature to see the times in a day when everyone is available.
-# To do this, you’ll need to know when any team is having a meeting. 
-# In HiCal, a meeting is stored as tuples ↴ of integers (start_time, end_time) 
-# These integers represent the number of 30-minute blocks past 9:00am.
 
 # Write a function merge_ranges() that takes a list of meeting time ranges and returns a list of condensed ranges
+# from operator import itemgetter
 
-# we could tell that the meetings overlapped because
-# 	end_time of the first meeting was after the start_time of the second meeting 
-# 	our ideas of first and second are important here - this only works 
-# 	after we ensure that we treat the meeting that starts earlier as the first one...
-
-meeting_times = [(1,3), (2,4)]
+meeting_times = [(1,3), (2,4), (0, 5)]
 
 def merge_ranges(meeting_times):
-	# get the start times - but they're not attached to 
-	start_times = [start[0] for start in meetings]
-	end_times = [end[1] for end in meetings]
 
-	# assign the first meeting 
-	first = min(start_times) 
+	# sorted() will put them in order
+	ordered_meeting_times = sorted(meeting_times)
+	print ordered_meeting_times
+	
+	# identify the first meeting
+	first = ordered_meeting_times[0]
+	print first
 
+	second = ordered_meeting_times[1]
+	print second
 
-	for meeting in meeting_times:
-		if end_time of first == start time of second
-			then merge meetings 
+	for meeting in ordered_meeting_times:
+# 		if end_time of first == start time of second
+		if first[1] == second[0]:
+# 			then merge meetings 
+
 		
-		if second ends before the first meeting ends [(1,5), (2,3)]
-			then merge the meetings
+# 		if second ends before the first meeting ends [(1,5), (2,3)]
+		if second[1] <= first[1]:
+# 			then merge the meetings
 
-		compare each tuple and earliest start_time = first
-		and other is second
+# 		compare each tuple and earliest start_time = first
+# 		and other is second
 
-		# if the end_time of first >= start_time of the second
-		# 	merge the two meetings into one time range
+# 		# if the end_time of first >= start_time of the second
+# 		# 	merge the two meetings into one time range
+
+# 		# 	the resulting time range's start time = 
+# 		# 	the first meeting's start_time and its end_time 
+# 		# 	is the later of the two meeitngs end times
+# 		else:
+# 			leave them separate
+
+# merge_ranges(meeting_times)
+
+# the meetings overlapped because end_time of the first is after the start_time of the second 
+# our ideas of first and second are important here
 
 
-		# 	the resulting time range's start time = 
-		# 	the first meeting's start_time and its end_time 
-		# 	is the later of the two meeitngs end times
-		else:
-			leave them separate
+# # meeting_times = [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]
+# # could we make the first and seconds into sets
+# firsts {1, 2, 3, 4}
+# seconds {2, 5, 6, 7}
+
+# and then do set math?
+# firsts & seconds = intersection {2, 3, 4}
+#  and the outliers mean, the start and ends?
 
 
-# could we make the first and seconds into sets
-firsts {1, 2, 3, 4}
-seconds {2, 5, 6, 7}
+	# # get the start times - but they're not attached to 
+	# start_times = [start[0] for start in meetings]
+	# end_times = [end[1] for end in meetings]
 
-and then do set math?
-firsts & seconds = intersection {2, 3, 4}
- and the outliers mean, the start and ends?
+	# # assign the first meeting based on lowest start time
+	# first = min(start_times) 
+
+	# # or sort the tuples by the first item, which is the start time - using itemgetter library? module?
+	# sorted(meetings, key=itemgetter(0))
