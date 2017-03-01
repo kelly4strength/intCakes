b92610a81be42ea22c9252ac714c4c6ab6b83f9b
Kelly Hoffer <kellyhoffer@Kellys-MacBook-Pro-2.local>
2017-03-01 12:28:10 -0800

    almost got things working, need to deal with how to skip if meeting time is within a meeting time added to the busy times list, may have to refactor

--- intCake4.py
+++ intCake4.py
@@ -2,39 +2,74 @@
 # Write a function merge_ranges() that takes a list of meeting time ranges and returns a list of condensed ranges
 # from operator import itemgetter
 
-meeting_times = [(1,3), (2,4), (0, 5)]
+meeting_times = [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]
+# meeting_times = [(1,3), (2,4)]
 
 def merge_ranges(meeting_times):
 
 	# sorted() will put them in order
 	ordered_meeting_times = sorted(meeting_times)
-	print ordered_meeting_times
-	
+
 	# identify the first meeting
-	first = ordered_meeting_times[0]
-	print first
+	# first = ordered_meeting_times[0]
+	# [(start_time, end_time)]
+	# merge meetings where start_time and end_time don't have an upper bound
+	# upper_bound = ordered_meeting_times[0][0]
 
-	second = ordered_meeting_times[1]
-	print second
+	busy_times = []
 
-	for meeting in ordered_meeting_times:
+	# for (start_time, end_time) in ordered_meeting_times:
+	for i in range((len(ordered_meeting_times)) -1):
+	# for meeting in ordered_meeting_times:
+		start_time = ordered_meeting_times[i][0]
+		end_time = ordered_meeting_times[i][1]
 
-		# shows the condensed ranges of meeting times
-		busy_times = []
+		if end_time < ordered_meeting_times[i+1][0]:
+			busy_times.append((start_time, end_time))
 
-# 		if end_time of first == start time of second
-		if first[1] == second[0]:
-# 			then merge meetings into one time range [(0, 5), (1,3), (2,4)]
-			busy_times.append((first[0], second[1]))
-
- 		if first[1] >= second[0]:
-			busy_times.append((first[0], first[1]))
-			print busy_times
-
-		else:
-			busy_times.append((first)(second))
+					# start_time of next meeting 
+		if end_time >= ordered_meeting_times[i+1][0]: 
+			busy_times.append((start_time, ordered_meeting_times[i+1][1]))
+		
+		# works except for 4, 8 whcih fits inside 3, 8 which was created before
+		# because i'm not comparing to the busy times list, just the ordered_meeting_times list
+		
+		
+		print busy_times
 
 merge_ranges(meeting_times)
+
+	# for i in range(len(ordered_meeting_times)):
+	# 	# print ordered[i], ordered[i + 1]
+	# 	# ordered_meeting_times[i] is the item (1, 3) then (2, 4)
+	# 	# if ordered_meeting_times[i] == ordered_meeting_times[i +1]
+	# 		# do nothing
+	# 	if ordered_meeting_times[i][1] >= [i+1][0]
+	# 		# merge
+	# 		busy_times.append((ordered_meeting_times[i][0], ordered_meeting_times[i]+1[1])
+
+# 		if first == meeting:
+# 			busy_times.append(meeting)
+
+# # 		if end_time of first >= start time of next
+# 		# then that meeting is within the bounds of the first and can be merged into it
+# 		if first[1] >= meeting[0]:
+# # 			then merge meetings into one time range [(0, 5), (1,3), (2,4)] (we totally lose (1,3) here)
+# 			busy_times.append((first[0], meeting[1]))
+
+# 		# if end time of first < start time of next, 
+
+#  		# if first[1] < meeting[0]:
+# 			# busy_times.append((meeting)
+# 			# # print busy_times
+
+# 		else:
+# 			# leave them separate
+# 			busy_times.append(meeting)
+
+# 		print busy_times
+
+# merge_ranges(meeting_times)
 
 
 # 	same as if first[1] >= second[0]:
